ce730426c3a51a8178c47d8fdf8668aad55d7fa4
abasava <basava.akula@gmail.com>
2021-02-10 18:42:17 +0530

    1. Working backup commit

--- NSE_option_chain_analyzer.py
+++ NSE_option_chain_analyzer.py
@@ -3,8 +3,11 @@
 import requests
 from typing import Union, Optional, List, Dict, Tuple, TextIO, Any
 from tkinter import Tk, Toplevel, Event, TclError, StringVar, Frame, Menu, \
-    Label, Entry, SOLID, RIDGE, N, S, E, W, LEFT, messagebox
-from tkinter.ttk import Combobox, Button, Notebook
+    Label, Entry, SOLID, RIDGE, N, S, E, W, LEFT, messagebox, IntVar, Scrollbar, RIGHT, BOTTOM, TOP, RIDGE,\
+    Listbox, END
+from tkinter.ttk import Combobox, Button, Notebook, Checkbutton
+from tkinter import ttk
+import tkinter as tk
 import pandas as pd
 import tksheet
 import nsepy.live as nse
@@ -41,12 +44,12 @@ class NSE:
             self.dict_dfs_STOCK[i] = pd.DataFrame()
         self.stop: bool = False
         self.curr_time = ""
-        self.interval = 30#seconds
+        self.interval = 5#seconds
         self.red: str = "#e53935"
         self.green: str = "#00e676"
         self.df_INDEX: pd.DataFrame = pd.DataFrame()
         self.df_STOCK: pd.DataFrame = pd.DataFrame()
-        self.popular_stocks: List[str] = ['ASHOKLEY','AXISBANK', 'BAJAJ-AUTO', 'BAJAJFINSV',\
+        self.popular_stocks: List[str] = ['TATASTEEL','ASHOKLEY','AXISBANK', 'BAJAJ-AUTO', 'BAJAJFINSV',\
                                           'BRITANNIA','DRREDDY','GLENMARK','HDFC', 'HDFCBANK',\
                                           'ICICIBANK','INDUSINDBK','INFY','MANAPPURAM','MARUTI',\
                                           'MUTHOOTFIN','RELIANCE','SBILIFE', 'SBIN','TATAMOTORS',\
@@ -91,7 +94,7 @@ class NSE:
         self.session: requests.Session = requests.Session()
         self.make_intial_nse_connection()
         self.expiry_date: String = ""
-        self.sheet_window(window)
+        self.setup_main_window(window)
         self.resposne: requests.Response = requests.Response()
     
     def make_intial_nse_connection(self)->None:
@@ -127,15 +130,22 @@ class NSE:
         self.stock_frame.columnconfigure(0, weight=1)
         self.stock_frame.pack(anchor=N,fill="both", expand=True)
         
-        self.SYMBS = self.indices
-        self.index_call = True
-        self.set_sheet(self.sh_frame)
-        self.sheet_formatting()
+        if(self.check_index_var.get()):
+            self.SYMBS = self.indices
+            self.index_call = True
+            self.set_sheet(self.sh_frame)
+            self.sheet_formatting()
         
-        self.SYMBS = self.stock_symbs
-        self.index_call = False
-        self.set_sheet(self.stock_frame)
-        self.sheet_formatting()
+        if(self.check_stocks_var.get()):
+            self.SYMBS = self.stock_symbs
+            #selected_symbs = []
+            #for i in range(len(self.stock_check_var)):
+            #    if(self.stock_check_var[i].get()):
+            #        selected_symbs.append(self.stock_check[i].text)
+            ##self.SYMBS.extend(selected_symbs)
+            self.index_call = False
+            self.set_sheet(self.stock_frame)
+            self.sheet_formatting()
     
     def set_sheet(self,my_frame: Frame)->None:
         self.NB: Notebook = Notebook(my_frame)
@@ -177,7 +187,7 @@ class NSE:
                     for j in range(num_cols-1,num_std_cols,-1):
                         diff = float(curr_sh.get_cell_data(i,j)) - float(curr_sh.get_cell_data(i,j-1))
                         perc_change = 1.
-                        if(float(curr_sh.get_cell_data(i,j-1)!=0.)):
+                        if(float(curr_sh.get_cell_data(i,j-1))>0.0):
                             perc_change = diff*100/float(curr_sh.get_cell_data(i,j-1))
                         if (diff<0.):
                             curr_sh.highlight_cells(row=i, column=j, bg=self.red,fg='white')
@@ -189,7 +199,8 @@ class NSE:
                             curr_sh.highlight_cells(row=i, column=j, bg='green',fg='white')
             curr_sh.set_currently_selected(0,num_cols-1)
             curr_sh.refresh()
-    def sheet_window(self,window)->None:
+    
+    def setup_main_window(self,window)->None:
         self.sh_window: Tk = window
         self.sh_window.title('Option chain analyzer')
         window_width: int = self.sh_window.winfo_reqwidth()
@@ -206,7 +217,8 @@ class NSE:
         top_frame: Frame = Frame(self.sh_window)
         top_frame.rowconfigure(0, weight=1)
         top_frame.columnconfigure(0, weight=1)
-        top_frame.pack(anchor=N, expand=False, side=LEFT)
+        top_frame.pack(anchor=S, expand=False, side=TOP)
+        
         
         row_idx: int = 0
         
@@ -215,12 +227,15 @@ class NSE:
         date_var: StringVar = StringVar()
         date_var.set(" ")
         lbl_exp_date: Label = Label(top_frame,text='Index Expiry',justify=LEFT,font=("TkDefaultFont", 10, "bold"))
-        lbl_exp_date.grid(row=row_idx,column=0,sticky=N+S+W)
-        self.date_combo_box = Combobox(top_frame,width=30,textvariable=date_var) 
-        self.date_combo_box.grid(row=row_idx, column=1, sticky=N + S + E + W)
+        #lbl_exp_date.grid(row=row_idx,column=0,sticky=N+S+W)
+        lbl_exp_date.pack(anchor=N, expand=False, side=LEFT)
+        self.date_combo_box = Combobox(top_frame,width=10,textvariable=date_var) 
+        self.date_combo_box.pack(anchor=N, expand=False, side=LEFT)
+        #self.date_combo_box.grid(row=row_idx, column=1, sticky=N + S + E + W)
         self.date_combo_box.bind('<<ComboboxSelected>>', self.set_expiry_date)
         self.date_combo_box['values'] = tuple(self.expiry_dates)
         self.date_combo_box.set(self.expiry_dates[0])
+        self.date_combo_box.configure(state='readonly')
         row_idx += 1
         
         self.index_call = False
@@ -228,28 +243,98 @@ class NSE:
         date_var_stock: StringVar = StringVar()
         date_var_stock.set(" ")
         lbl_exp_date_stock: Label = Label(top_frame,text='Stock Expiry',justify=LEFT,font=("TkDefaultFont", 10, "bold"))
-        lbl_exp_date_stock.grid(row=row_idx,column=0,sticky=N+S+W)
-        self.date_combo_box_stock = Combobox(top_frame,width=30,textvariable=date_var_stock) 
-        self.date_combo_box_stock.grid(row=row_idx, column=1, sticky=N + S + E + W)
+        #lbl_exp_date_stock.grid(row=row_idx,column=0,sticky=N+S+W)
+        lbl_exp_date_stock.pack(anchor=N, expand=False, side=LEFT)
+        self.date_combo_box_stock = Combobox(top_frame,width=10,textvariable=date_var_stock) 
+        self.date_combo_box_stock.pack(anchor=N, expand=False, side=LEFT)
+        #self.date_combo_box_stock.grid(row=row_idx, column=1, sticky=N + S + E + W)
         self.date_combo_box_stock.bind('<<ComboboxSelected>>', self.set_expiry_date)
         self.date_combo_box_stock['values'] = tuple(self.expiry_dates)
         self.date_combo_box_stock.set(self.expiry_dates[0])
+        self.date_combo_box_stock.configure(state='readonly')
+        row_idx += 1
+
+        self.check_stocks_var = IntVar()
+        self.check_stocks = Checkbutton(top_frame, text = "Stocks", variable = self.check_stocks_var, \
+                                    onvalue = 1, offvalue = 0, width = 10)
+        #self.check_stocks.grid(row=row_idx, column=1, sticky=N + S + E + W)
+        self.check_stocks.pack(anchor=N, expand=False, side=LEFT)
+        self.check_stocks_var.set(0)
+        row_idx += 1
+        
+        self.check_index_var = IntVar()
+        self.check_index = Checkbutton(top_frame, text = "Index", variable = self.check_index_var, \
+                                    onvalue = 1, offvalue = 0, width = 10)
+        #self.check_index.grid(row=row_idx, column=1, sticky=N + S + E + W)
+        self.check_index.pack(anchor=N, expand=False, side=LEFT)
+        self.check_index_var.set(1)
         row_idx += 1
         
         ref_intvl: Stringvar = StringVar()
         ref_intvl.set(" ")
         lbl_refresh_interval: Label = Label(top_frame,text='Interval (min)',justify=LEFT,font=("TkDefaultFont", 10, "bold"))
-        lbl_refresh_interval.grid(row=row_idx,column=0,sticky=N+S+W)
-        self.ref_intvl_cbox: Combobox = Combobox(top_frame,width=30,textvariable=ref_intvl)
-        self.ref_intvl_cbox.grid(row=row_idx, column=1, sticky=N + S + E + W)
+        #lbl_refresh_interval.grid(row=row_idx,column=0,sticky=N+S+W)
+        lbl_refresh_interval.pack(anchor=N, expand=False, side=LEFT)
+        self.ref_intvl_cbox: Combobox = Combobox(top_frame,width=10,textvariable=ref_intvl)
+        #self.ref_intvl_cbox.grid(row=row_idx, column=1, sticky=N + S + E + W)
+        self.ref_intvl_cbox.pack(anchor=N, expand=False, side=LEFT)
         self.ref_intvl_cbox.bind('<<ComboboxSelected>>', self.set_ref_intvl)
         self.ref_intvl_cbox['values'] = tuple(range(1,10,1))
         self.ref_intvl_cbox.configure(state='readonly')
         row_idx += 1
 
-        self.start_button: Button = Button(top_frame,text='START',command=self.main_recursive,width=3)
-        self.start_button.grid(row=row_idx, column=1, sticky=N + S + E + W)
+        self.start_button: Button = Button(top_frame,text='START',command=self.main_recursive,width=10)
+        #self.start_button.grid(row=row_idx, column=1, sticky=N + S + E + W)
+        self.start_button.pack(anchor=N, expand=True, side=TOP)
         row_idx += 1
+        
+        canvas = tk.Canvas(self.sh_window)
+        scroll_y = tk.Scrollbar(self.sh_window, orient="vertical", command=canvas.yview)
+        
+        bot_frame: Frame = Frame(canvas)
+        #bot_frame = ScrollFrame(bot_frame)
+        #bot_frame.rowconfigure(0, weight=1)
+        #bot_frame.columnconfigure(0, weight=1)
+        #bot_frame.grid(row=row_idx,column=0,sticky=N+S+W)
+        #bot_frame = ScrollableFrame(self.sh_window)
+        #bot_frame = ScrollFrame(self.sh_window)
+        #bot_frame.rowconfigure(1, weight=1)
+        #bot_frame.columnconfigure(0, weight=1)
+        #bot_frame.pack(anchor=N, expand=False, side=LEFT)
+
+        
+        #bot_frame: Listbox = Listbox(self.sh_window)
+        #bot_frame.pack(side=LEFT,fill='both')
+        #vscrollbar = Scrollbar(self.sh_window)
+        #vscrollbar.pack(side = LEFT, fill = 'both')
+        #for i in range(10000):
+        #    bot_frame.insert(END,i)
+        
+        self.stock_check_var: List[IntVar]  = []
+        self.stock_check: List[Checkbutton] = []
+        int_col = 0
+        tmp_row_idx = row_idx
+        for stk in enumerate(self.stock_symbs):
+            #if(int(stk[0])>30 and int_col==0):
+            #    int_col = 1
+            #    row_idx = tmp_row_idx
+            self.stock_check_var.append(IntVar())
+            cb = Checkbutton(bot_frame, text = stk[1], variable = self.stock_check_var[-1], \
+                                    onvalue = 1, offvalue = 0, width =12)
+            cb.pack()
+            if(stk[1] in self.popular_stocks):
+                self.stock_check_var[-1].set(1)
+            else:
+                self.stock_check_var[-1].set(0)
+            self.stock_check.append(cb)
+            row_idx += 1
+        canvas.create_window(0, 0, anchor='nw', window=bot_frame) 
+        canvas.update_idletasks() 
+        canvas.configure(scrollregion=canvas.bbox('all'), 
+                 yscrollcommand=scroll_y.set)
+                 
+        canvas.pack(fill='y', expand=False, side=LEFT)
+        scroll_y.pack(fill='y', side=LEFT,expand=False)
 
         self.sh_window.mainloop()
     
@@ -278,7 +363,7 @@ class NSE:
         if self.index_call:
             self.stock_symb = 'NIFTY'
         else:
-            self.stock_symb = 'ASHOKLEY'
+            self.stock_symb = 'INFY'
         self.get_option_chain_data() 
         json_data = self.response.json()
         self.expiry_dates: List = []
